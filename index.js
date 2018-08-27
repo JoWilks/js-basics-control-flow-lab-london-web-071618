9da3e45f6453090c15a7db2fef8a378585454579
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-08-27 10:45:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function ternaryCheckCity(city) {
   return (city == 'NYC' ? 'Ok, sounds good.' : 'No go.');
 }
 
-function switchOnCharmFromTip() {
-  
+function switchOnCharmFromTip(tip) {
+  if (tip === 'generous') { }
 }
