a6e4d016848df21437845fbfed3f259e4b3ee9d7
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-08-27 10:37:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // Write your code in this file!
 
 function scuberGreetingForFeet(feet) {
-if (feet <= 400) {
-  return "This one is on me!"
+if (feet > 2500) {
+  return "Nooope"
 } else if  (feet > 2000) {
   return 'I will gladly take your thirty bucks.'
-} else if 
+} else if (feet <= 400) {
+  return "This one is on me!"
+}
 }
