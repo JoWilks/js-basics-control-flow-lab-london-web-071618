15819eb32422f4d22bc60e88e8eea199edc63b9e
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-08-27 10:41:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ else if (feet <= 400) {return "This one is on me!"}
 }
 
 function ternaryCheckCity(city) {
-  city === 'NYC' ? "Ok, sounds good." : "No go.";
+  city === 'NYC' ? return "Ok, sounds good." : return "No go.";
 }
